830a2fbe180688cb136dbb3c8ba3b3cf45f0bd6a
Anshul <anshul524@gmail.com>
2020-05-12 21:44:57 +0530

    array performance test case

--- src/test/java/com/in28minutes/junit/helper/ArraysCompareTest.java
+++ src/test/java/com/in28minutes/junit/helper/ArraysCompareTest.java
@@ -21,5 +21,14 @@ public class ArraysCompareTest {
 		int[] numbers = null;
 		Arrays.parallelSort(numbers);
 	}
+	
+	@Test(timeout = 1000)
+	public void test_Array_Performance() {
+		int[] array = {12,23,4};
+		for (int i = 1; i <= 100000; i++) {
+			array[0] = i;
+			Arrays.sort(array);
+		}
+	}
 
 }
